bf2675aa452f2101d726a1ab5f3b1ee247182136
Do Bryan <110252@eesd.org>
2021-01-11 23:57:58 +0000

    What didn't you change?

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello World") # This print statement prints out 'Hello World'
+
+# Write 5 print statements
+# Name, Your grade, favorite color, favorite sport, favorite ice cream flavor
+print("My name is Bryan Do")
+print("I am in 7th Grade")
+print("My favorite color is Blue")
+print("I don't really have a favorite sport as of right now")
+print("My favorite ice cream flavor is vanilla")
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Output
+
+Output is the way the computer prints information to the user in the "Console" 
+
+To output we use `print` 
+
+## Comments
+
+Comments are notes that the compiler skips over
+
+To comment we use `#`
